e7ddc0da35a97c47d76d55ad75047ca4986019cc
dewesh.tripathi <dewesh83@gmail.com>
2016-10-19 18:07:22 +0530

    First Commit

---
+++ app/src/main/java/smartconnect/dst/com/smartconnect/MainActivity.java
@@ -0,0 +1,13 @@
+package smartconnect.dst.com.smartconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
